aaf807d751bbabf059158e0846e5f9abf66a4138
Slackow <32826498+Slackow@users.noreply.github.com>
2025-03-29 12:47:16 -0400

    add example route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
-import * as express from 'express';
+import express from 'express';
+
 let app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send("Hello World!")
 });
-
+app.get('/professor/:name', (req, res) => {
+  let name = req.params.name;
+  res.send(`Names: ${name}`)
+});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
