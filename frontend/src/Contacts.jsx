16145f5685bc662bf062993fdb7f4e749d813995
RJohnson2106 <privateryan112106@gmail.com>
2025-03-29 19:33:11 -0400

    Added in Contact page
    -Has images, GitHub links, and LinkedIn links for all 4 of us

--- frontend/src/Contacts.jsx
+++ frontend/src/Contacts.jsx
@@ -1,10 +1,94 @@
 import './Contacts.css';
+import Ryan from '../images/Ryan.jpg';
+import Marvin from '../images/Marvin.jpg';
+import Miller from '../images/Miller.jpg';
+import Andrew from '../images/Andrew.jpg';
 
 function Contacts() {
   return (
     <div className="fade-container">
-      <h2>Contact Page</h2>
-      <p>Your contact details go here...</p>
+      <div className="images-container">
+        <div className="image-item">
+          <img src={Ryan} alt="Ryan" />
+          <p className="name">Ryan</p>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/ryan-johnson-559b822b6/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/RJohnson2106"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="image-item">
+          <img src={Marvin} alt="Marvin" />
+          <p className="name">Marvin</p>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/marvinrm/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/marvinmody"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="image-item">
+          <img src={Miller} alt="Miller" />
+          <p className="name">Miller</p>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/miller-paule-b22b34331/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/pewpewpewman"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="image-item">
+          <img src={Andrew} alt="Andrew" />
+          <p className="name">Andrew</p>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/andrew-turcan/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/Slackow"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
